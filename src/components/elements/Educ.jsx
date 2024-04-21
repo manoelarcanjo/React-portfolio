ea3cfb6b2517d6d7e6effdda7bc4c5d17bf9394d
Manoel Arcanjo <manoel199.333@gmail.com>
2024-04-21 11:56:40 -0300

    Criando e estilizando a Education.jsx

--- src/components/elements/Educ.jsx
+++ src/components/elements/Educ.jsx
@@ -1,11 +1,22 @@
 import styles from '../../styles/Educ/Educ.module.css'
+import PropTypes from 'prop-types'
 
-function Educ () {
+function Educ ({img, curso, escola, ano }) {
     return (
         <div className={styles.card}>
-
+            <img src={img} alt="" />
+            <h3>{curso}</h3>
+            <h4>{escola}</h4>
+            <p>{ano}</p>
         </div>
     )
+}
+
+Educ.propTypes = {
+    img: PropTypes.string.isRequired,
+    curso: PropTypes.string.isRequired,
+    escola: PropTypes.string.isRequired,
+    ano: PropTypes.number.isRequired,
 }
 
 export default Educ
--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,7 +1,11 @@
+import Educ from "../../components/elements/Educ";
 import Title from "../../components/elements/Title";
 import Footer from "../../components/sections/Footer";
 import Header from "../../components/sections/Header";
 import styles from '../../styles/Education/Education.module.css'
+import uninassau from '../../assets/images/uninassau.svg'
+import dnc from '../../assets/images/dnc.svg'
+import vainaweb from '../../assets/images/vainaweb.svg'
 
 function Education () {
     return (
@@ -12,7 +16,9 @@ function Education () {
                 <Title title="Education"/>
 
                 <div className={styles.education}>
-
+                    <Educ img={uninassau} curso="Análise e Desenvolvimento de Sistemas" escola="Uninassau" ano={'2024 - 2025'}/>
+                    <Educ img={vainaweb} curso="Desenvolvimento Front-end" escola="Vai na Web" ano={'2023 - 2024'}/>
+                    <Educ img={dnc} curso="Formação em Tecnologia" escola="Uninassau" ano={'2024 - 2025'}/>
                 </div>
             </div>
 
